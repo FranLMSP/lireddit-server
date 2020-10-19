08997a0e37bec44fe37075c9aec3abc8537dc2cf
Franco Colmenarez <francoacg1@gmail.com>
2020-10-18 19:11:09 -0500

    Mikro-ORM setup

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import { MikroORM } from "@mikro-orm/core";
-import { Post } from "./entities/Post"
+// import { Post } from "./entities/Post"
 import microConfig from "./mikro-orm.config";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
 
-  const post = orm.em.create(Post, {title: 'My first post'});
-  await orm.em.persistAndFlush(post);
+  // const post = orm.em.create(Post, {title: 'My first post'});
+  // await orm.em.persistAndFlush(post);
+  // const post = await orm.em.find(Post, {});
+  // console.log(post)
 }
 
 main().catch( err => {
